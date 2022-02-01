af332a9e987da074008629767ac6217a760e0418
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2022-02-01 14:20:34 +0100

    npm-init: hotfix PATCH allowed

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'dev') {
     app.use(function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
       next();
       })
