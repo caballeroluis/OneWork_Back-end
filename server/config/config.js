e656f8515a0843fb494e77be908f9fca720ae091
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2022-05-31 02:32:13 +0200

    npm-init: environments hotfix

--- server/config/config.js
+++ server/config/config.js
@@ -26,21 +26,5 @@ if(process.env.NODE_ENV === 'dev') {
       })
 }
 
-if(process.env.NODE_ENV === 'pre') {
-    app.use(function(req, res, next) {
-      next();
-      })
-}
-
-if(process.env.NODE_ENV === 'pro') {
-    app.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-      next();
-      })
-}
-
 
 module.exports = app;
